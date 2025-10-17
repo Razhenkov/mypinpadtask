b4741ffd2b350f9368dd5a2c5149bfcaff5b5d4c
Blagovest Razhenkov <razhenkov@gmail.com>
2025-10-17 23:14:15 +0300

    fix: handling crypto exception during integrity check

--- src/MyPinPad.Core/Validators/IntegrityValidators/HmacSha256IntegrityValidator.cs
+++ src/MyPinPad.Core/Validators/IntegrityValidators/HmacSha256IntegrityValidator.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using MyPinPad.Core.Exceptions;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace MyPinPad.Core.Validators.IntegrityValidators
@@ -14,16 +15,30 @@ namespace MyPinPad.Core.Validators.IntegrityValidators
 
         public string Compute(string data)
         {
-            using var hmac = new HMACSHA256(_key);
+            try
+            {
+                using var hmac = new HMACSHA256(_key);
 
-            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
-            return Convert.ToBase64String(hash);
+                var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+                return Convert.ToBase64String(hash);
+            }
+            catch (CryptographicException ex)
+            {
+                throw new CryptoException("An error occurred while computing integrity", ex);
+            }
         }
 
         public bool Verify(string data, string signature)
         {
-            var computed = Compute(data);
-            return computed.Equals(signature, StringComparison.OrdinalIgnoreCase);
+            try
+            {
+                var computed = Compute(data);
+                return computed.Equals(signature, StringComparison.OrdinalIgnoreCase);
+            }
+            catch (CryptographicException ex)
+            {
+                throw new CryptoException("An error occurred while verifying integrity", ex);
+            }
         }
     }
 }
